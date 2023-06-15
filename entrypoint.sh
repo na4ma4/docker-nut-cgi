2cfc358d384026fb17cb11e8b76e40bb93e4632f
na4ma4 <na4ma4@users.noreply.github.com>
2023-06-15 17:09:44 +1000

    added authentication support

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,4 +11,14 @@ do
     echo "${host}" >> /etc/nut/hosts.conf
 done
 
+if [ ! -z ${NUT_AUTH} ]; then
+    echo "${NUT_AUTH}" > /etc/lighttpd/lighttpd.user
+    (
+        echo 'server.modules += ( "mod_auth", "mod_authn_file" )'
+        echo 'auth.backend = "plain"'
+        echo 'auth.backend.plain.userfile  = "/etc/lighttpd/lighttpd.user"'
+        echo 'auth.require = ( "" => ("method" => "basic", "realm" => "example", "require" => "valid-user") )'
+    ) > "/etc/lighttpd/conf-enabled/05-auth.conf"
+fi
+
 /usr/sbin/lighttpd -D -f /etc/lighttpd/lighttpd.conf
